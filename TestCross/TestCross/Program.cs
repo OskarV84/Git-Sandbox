01623801a2ecd3a4a4e62b2ff4eaac7c788257a0
Oskar V. Gislason <ovg@ramboll.dk>
2019-06-22 16:30:36 +0200

    C# Console app project added

---
+++ TestCross/TestCross/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCross
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /// Git test project
+            /// Try to setup a clone of this repository and add a Console.Writeline("Success!!!") to the code and 
+            /// push the solution to the repository.
+
+            Console.ReadLine();
+
+        }
+    }
+}
